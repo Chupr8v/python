c5b3fc79019a3254e732c457c6de76e636e4e704
Alex <alexey.chuprov.x@gmail.com>
2023-01-05 12:30:59 +0300

    complete mission

---
+++ python/lec.py
@@ -0,0 +1,64 @@
+#print('Hello Python')
+#value = None
+#a = 123
+#b = 1.23
+#print (type(a))
+#print (a)
+#print (b)
+#value =12334
+#print (value)
+#str = "'heelllooo'"
+#print(str) # вывод строки
+#print ('{1} - {2} - {0}'.format(a, b, str))
+#print (f'{a} - {b} - {str}')
+
+#f = True
+#print(f)
+
+#list = ['1', '2', '23']
+#col = ['1', '2', '23']
+#print(list)
+#print(col)
+
+#print('Введите a')
+#a = int(input())
+#print('Введите b')
+#b = int(input())
+#print(a, ' + ' , b, ' = ', a+b)
+
+#a = 1.31231223
+#b = 3
+#c = round(a * b, 7)
+#print(c)
+
+#a = 3
+#a *= 5
+
+#print(a)
+ 
+# Логические операции
+#>, >=, <, <=, ==, !=
+#not, and, or – не путать с &, |,^
+#Кое-что ещё: is, is not, in, not in
+
+#f = 2<1 and 2>1
+
+#print(f)
+
+#Управляющие конструкции
+#a = int(input('a = '))
+#b = int(input('b = '))
+#if a > b:
+#    print(a)
+#else:
+#    print(b)    
+
+#username = input('Введите имя: ')
+#if username == 'Evlyv':
+   # print('Привет, Любимая!')
+#elif username == 'Макс':
+ #   print('Дроу, чандун')   
+#elif username == 'Кирилл':
+    #print('Привеt')    
+#else:
+   # print('Привет, ', username)         
